33a6c2c6f3c4cbe264aa08e0b446f420fe96f874
oladims <olufoworaibrahim@gmail.com>
2019-11-21 14:08:55 +0100

    Update views

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const msgModal = ` <div class="modal fade" id="exampleLoginModal" tabindex="-1" 
 <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
   <div class="modal-content">
     <div class="modal-header">
-      <h5 class="modal-title bolder" id="exampleModalLabel">Oooops</h5>
+      <h5 class="modal-title bolder" id="exampleModalLabel">Hello there!</h5>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
